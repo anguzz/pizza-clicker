75a8d9db0a0a005de7de38472c64bb5758975ff2
Angel <santoyox714@gmail.com>
2022-08-05 02:24:55 -0700

    update

--- src/lib/deps.js
+++ src/lib/deps.js
@@ -1,4 +1,3 @@
-import randomWords from 'random-words'
 
 export { seedPRNG, random } from '@georgedoescode/generative-utils';
 
@@ -6,5 +5,5 @@ import colors from 'nice-color-palettes/100';
 
 import tinycolor from 'tinycolor2';
 
-export { colors, tinycolor, randomWords };
+export { colors, tinycolor };
 
